ce5323209fe554931d28d6551ce8971d85b19a85
Xutaotaotao <xutaotao852@hellobike.com>
2023-10-10 16:27:29 +0800

    feat: add build and compile config

--- src/render/App.tsx
+++ src/render/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Logo from './assets/logo.svg'
+import { sendMsgToMain } from 'electron-prokit'
 import './App.css'
 
 function App() {
@@ -8,15 +8,15 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div onClick={() => sendMsgToMain({
+        key:'openUrl',
+        data: {
+          url:'https://xutaotaotao.github.io/electron-prokit/'
+        }
+      })}>
+          <img src={Logo} className="logo" alt="Vite logo" />
       </div>
-      <h1>Vite + React</h1>
+      <p>A vite + react + electron template with electron-prokit.</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the logo to learn more
       </p>
     </>
   )
